52d2ca66ff34c1c0734c8472bba1620b3476b021
VitaliyDorosh <vitaliy.dorosh@gmail.com>
2021-12-03 15:03:49 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+SUNRISE = datetime.strptime('06:00', '%H:%M')
+SUNSET = datetime.strptime('18:00', '%H:%M')
+
+
+def sun_angle(time, sunrise=SUNRISE, sunset=SUNSET):
+    time = datetime.strptime(time, '%H:%M')
+    if not sunrise <= time <= sunset:
+        return "I don't see the sun!"
+
+    angle_per_hour = 180 / (sunset - sunrise).seconds * 3600
+    time_since_sunrise = (time - sunrise).seconds / 3600
+    return time_since_sunrise * angle_per_hour
+
+
+if __name__ == "__main__":
+ x = '17:59'
+ output = sun_angle(x)
+
+ print(output)
+
